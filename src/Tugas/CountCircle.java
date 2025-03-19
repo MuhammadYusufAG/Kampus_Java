80e8671ab077b08d2f05b3cb0983ce71e5a85fd2
Muhammad Yusuf A G <ghanyyusuf33@gmail.com>
2025-03-19 19:02:59 +0700

    feat: Adding Tugas menghitung lingkaran

---
+++ src/Tugas/CountCircle.java
@@ -0,0 +1,34 @@
+package Tugas;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class CountCircle {
+    public static void main(String[] args) {
+
+        Scanner Jari_Jari = new Scanner(System.in);
+
+        Integer r , diameter;
+        double Luas, Keliling, pi = 3.14;
+
+        System.out.println("Program lingkaran java");
+        System.out.println("======================");
+
+        //input
+        System.out.println("Jari-Jari: ");
+        r = Jari_Jari.nextInt();
+
+        //Process or rumus
+        diameter = 2 * r;
+        Keliling = pi * diameter;
+        Luas = pi * r * r;
+
+        //output
+        System.out.println("Diameter: " + diameter);
+        System.out.println("Luas: " + Luas);
+        System.out.println("Keliling: " + Keliling);
+
+
+
+
+    }
+}
